d101c17d5a29defe09bcf3db55ce9448deda8bcf
Thomas Maurin <maurin.th@gmail.com>
2014-10-06 15:19:14 +0000

    Added a way to ensure registered emails are unique + unit tests

--- naxos/user/forms.py
+++ naxos/user/forms.py
@@ -3,13 +3,23 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
+# TODO: add email confirmation
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    # confirm = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        "Ensures registered emails are unique."
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(
+                username=username).count():
+            raise forms.ValidationError('Adresse déjà enregistrée.')
+        return email
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
---
+++ naxos/user/test_forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from .forms import RegisterForm
+
+
+class RegistrationTest(TestCase):
+
+    def setUp(self):
+        User.objects.create_user(username='jacob',
+            email='jacob@test.com', password='top_secret')        
+
+    def test_email_in_use(self):
+        form_data = {
+                     'username':'test',
+                     'email':'jacob@test.com',
+                     'password1':'top_secret',
+                     'password2':'top_secret'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertEqual(form.errors['email'], ['Adresse déjà enregistrée.'])
+
+    def test_valid_input(self):
+        form_data = {
+                     'username':'test',
+                     'email':'other@test.com',
+                     'password1':'top_secret',
+                     'password2':'top_secret'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertEqual(form.is_valid(), True)

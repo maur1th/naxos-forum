edce4625e33c7682819cdfbace6304f79ca1c554
Thomas Maurin <maurin.th@gmail.com>
2014-10-16 16:08:16 +0200

    Added reply view and template, tweaked some unnecessary code

--- naxos/forum/models.py
+++ naxos/forum/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
 import datetime
 from uuslug import uuslug
--- naxos/forum/views.py
+++ naxos/forum/views.py
@@ -52,7 +52,15 @@ class NewThread(LoginRequiredMixin, CreateView):
     form_class = NewThreadForm
     template_name = 'forum/new_thread.html'
 
+    def get_context_data(self, **kwargs):
+        "Pass Category from url to context"
+        context = super(NewThread, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.get(
+            slug=self.kwargs['category_slug'])
+        return context
+
     def form_valid(self, form):
+        """ Handle thread and 1st post creation in the db"""
         thread = Thread()  # Create the Thread
         thread.title = self.request.POST['title']
         thread.author = self.request.user
@@ -64,13 +72,6 @@ class NewThread(LoginRequiredMixin, CreateView):
         form.instance.save()
         return HttpResponseRedirect(self.get_success_url())
 
-    def get_context_data(self, **kwargs):
-        "Pass Category from url to context"
-        context = super(NewThread, self).get_context_data(**kwargs)
-        context['category'] = Category.objects.get(
-            slug=self.kwargs['category_slug'])
-        return context
-
     def get_success_url(self):
         return reverse_lazy('forum:category', kwargs=self.kwargs)
 
@@ -79,3 +80,23 @@ class NewPost(LoginRequiredMixin, CreateView):
     model = Post
     fields = ('content_plain',)
     template_name = 'forum/new_post.html'
+
+    def get_context_data(self, **kwargs):
+        "Pass category and thread from url to context"
+        context = super(NewPost, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.get(
+            slug=self.kwargs['category_slug'])
+        context['thread'] = Thread.objects.get(
+            slug=self.kwargs['thread_slug'])
+        return context
+
+    def form_valid(self, form):
+        """ Handle post creation in the db"""
+        form.instance.thread = Thread.objects.get(
+            slug=self.kwargs['thread_slug'])
+        form.instance.author = self.request.user
+        form.instance.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy('forum:thread', kwargs=self.kwargs)

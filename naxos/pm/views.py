28b1b6bf021156093657e603bb20b92508628fd2
Thomas Maurin <maurin.th@gmail.com>
2014-11-28 01:45:01 +0100

    Tweaks

--- naxos/pm/views.py
+++ naxos/pm/views.py
@@ -128,7 +128,8 @@ def GetConversation(request):
                 return HttpResponseRedirect(reverse_lazy('pm:msg',
                     kwargs={'pk': c.pk}) + '#' + str(c.messages.latest().pk))
             else:
-                messages.error(request, "Il n'existe pas de conversation avec cet utilisateur : {:s}.".format(u.get().username))
+                messages.error(request, ("Il n'existe pas de conversation avec"
+                     " cet utilisateur : {:s}.".format(u.get().username)))
         elif u.count() > 1:
             messages.error(request,
                 "Plusieurs utilisateurs possibles : {:s}.".format(

6f5424626ef7280bf03ae73600ce7e41292d3e20
Thomas Maurin <maurin.th@gmail.com>
2014-12-03 11:51:01 +0100

    Updated settings

--- naxos/naxos/settings/base.py
+++ naxos/naxos/settings/base.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'naxos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 

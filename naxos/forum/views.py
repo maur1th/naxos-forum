2a30f09d300c2d8035b0b7a29da241636f46fb8a
Thomas Maurin <maurin.th@gmail.com>
2014-12-10 22:28:53 +0100

    Added initial code, not functional though.

--- naxos/forum/views.py
+++ naxos/forum/views.py
@@ -11,9 +11,25 @@ from .models import Category, Thread, Post, Preview, PollQuestion, \
     ThreadCession
 from .forms import ThreadForm, PostForm, PollThreadForm, QuestionForm, \
     ChoicesFormSet, FormSetHelper
-
+from .util import get_query
 
 MERGEPOST_INTERVAL = 300
+
+
+### Search stuff ###
+def search(request):
+    query_string = ''
+    found_entries = None
+    if ('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET['q']
+        entry_query = get_query(query_string, ['title',])
+        found_entries = Thread.objects.filter(entry_query)\
+                                     .order_by('-pub_date')
+
+    return render_to_response('forum/search_results.html',
+                              {'query_string': query_string,
+                               'found_entries': found_entries},
+                              context_instance=RequestContext(request))
 
 
 ### Helpers ###

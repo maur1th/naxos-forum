88d2fe33e852801a3f4448f5dbb1a293dd8762f7
Thomas Maurin <maurin.th@gmail.com>
2014-12-11 23:10:08 +0100

    Tweaked search view.

--- naxos/forum/views.py
+++ naxos/forum/views.py
@@ -382,4 +382,5 @@ class SearchView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['model'] = self.object_list.model._meta.model_name
+        context['query'] = self.query
         return context

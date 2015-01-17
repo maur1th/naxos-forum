fc44656df7970e96f1f04aa91f53d60740de14e2
Thomas Maurin <maurin.th@gmail.com>
2015-01-17 18:33:29 +0100

    Various enhancements.

--- naxos/migrate.py
+++ naxos/migrate.py
@@ -6,10 +6,10 @@ import json
 import django
 from html.parser import HTMLParser
 from datetime import datetime
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "naxos.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "naxos.settings.dev")
 django.setup()
 
-from naxos.settings.local import here
+from naxos.settings.base import here
 from user.models import ForumUser
 from forum.models import Category, Thread, Post
 from forum.util import keygen
@@ -36,7 +36,10 @@ def fix_json(f):
     s = s.replace('<br />', '\\n')  # User proper new line character
     s = s.replace('\\\'', '\'')  # Remove illegal escapes for squotes
     s = re.sub(r'[^\x20-\x7e]', '', s)  # remove hex characters
-    s = re.sub(r'("msg": )([^"]*),', double_quote, s)  # +missing dquotes
+    # add double quotes where missing
+    s = re.sub(r'("msg": )([^"]*),', double_quote, s)
+    s = re.sub(r'("usercitation": )([^"]*),', double_quote, s)
+    # let's roll
     print("All right, good to go.")
     return s
 
@@ -48,17 +51,15 @@ def convert_username(name):
 
 def import_users(f):
     """Import users from a CoolForum json db extract"""
-    f = open(f)
-    users = json.loads(f.read())
+    users = json.loads(fix_json(f))
+    new_users = {}
 
     for i, user in enumerate(users):
         user['login'] = convert_username(user['login'])
         m = ForumUser.objects.filter(pk=user['userid']).first()
-        if m: m.delete()
-        m = ForumUser.objects.filter(
-            username=HTMLParser().unescape(user['login'])).first()
-        if m: m.delete()
-        password = keygen()
+        if m: m.delete()  # delete the user if it already exists
+        password = keygen()  # gen password
+        new_users[user['login']] = password
         u = ForumUser.objects.create(
             pk=int(user['userid']),
             username=user['login'],
@@ -68,11 +69,15 @@ def import_users(f):
             quote=HTMLParser().unescape(str(user['usercitation'])),
             website=HTMLParser().unescape(str(user['usersite'])),
         )
-        print("Creating {:d}/{:d}: {:s} with pwd {:s}".format(
-            i+1, len(users), u.username, password))
+        print("Creating users... {:d}/{:d}: {:s}{:s}".format(
+            i+1, len(users), u.username, " "*(30-len(u.username))), end="\r")
         u.set_password(password)
         u.save()
-        # TODO: send email with new password
+    print()
+    f = open('new_users.json', 'w')
+    json.dump(new_users, f)
+    f.close()
+    # TODO: send email with new password
 
 
 def import_threads(f):
@@ -80,7 +85,7 @@ def import_threads(f):
     threads = json.loads(fix_json(f))
     for i, thread in enumerate(threads):
         m = Thread.objects.filter(pk=thread['idtopic']).first()
-        if m: m.delete()
+        if m: m.delete()  # delete the thread if it already exists
         t = Thread.objects.create(
             pk=int(thread['idtopic']),
             category=Category.objects.get(pk=cat_map[thread['idforum']]),
@@ -89,8 +94,9 @@ def import_threads(f):
             icon=str(thread['icone'])+'.gif',
             viewCount=int(thread['nbvues']),
         )
-        print("Creating {:d}/{:d}".format(i+1, len(threads)))
-
+        print("Creating threads... {:d}/{:d}".format(
+            i+1, len(threads)), end="\r")
+    print()
 
 def import_posts(f):
     posts = json.loads(fix_json(f))
@@ -110,8 +116,8 @@ def import_posts(f):
         if not t: continue  # pass if thread does not exist
         # increment category post counter
         post_counter[t.category] = post_counter.get(t.category, 0) + 1
-        m = Post.objects.filter(pk=post['idpost']).first()
-        if m: continue  # skip loop if post already exists
+        # m = Post.objects.filter(pk=post['idpost']).first()
+        # if m: continue  # skip if post already exists
         p = Post.objects.create(
             pk=int(post['idpost']),
             thread=t,
@@ -119,13 +125,13 @@ def import_posts(f):
             created=datetime.fromtimestamp(post['date']),
             content_plain=str(post['msg']),
         )   
-        print("Creating {:d}/{:d}".format(i+1, len(posts)))
-        if i == 1000:
-            return
+        print("Creating {:d}/{:d}".format(i+1, len(posts)), end="\r")
+        # if i == 1000:
+        #     return
     threads = Thread.objects.all()
     for i, thread in enumerate(threads):
         print('Updating thread modified datetime: {:d}/{:d}'.format(
-            i+1, len(threads)))
+            i+1, len(threads)), end="\r")
         thread.modified = thread.posts.latest.modified
         thread.save()
     for key, value in post_counter:
@@ -138,6 +144,6 @@ def import_posts(f):
 # TODO: add count posts
 # TODO: override thread.modified with last topic datetime
 
-import_users(here('..', '..', '..', 'util', 'data', 'new.json'))
-import_threads(here('..', '..', '..', 'util', 'data', 'CF_topics.json'))
-# import_posts(here('..', '..', '..', 'util', 'data', 'CF_posts.json'))
+# import_users(here('..', '..', '..', 'util', 'data', 'CF_user.json'))
+# import_threads(here('..', '..', '..', 'util', 'data', 'CF_topics.json'))
+import_posts(here('..', '..', '..', 'util', 'data', 'CF_posts.json'))

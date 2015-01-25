5bec44138d8296641d79ed08b7fe2f989660da0a
Thomas Maurin <maurin.th@gmail.com>
2015-01-25 11:08:48 +0100

    Updated base.html to use 3rd party CDN for jquery and bootstrap

--- naxos/forum/views.py
+++ naxos/forum/views.py
@@ -48,6 +48,9 @@ class ThreadView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         "Pass category from url to context"
         context = super().get_context_data(**kwargs)
+        # for t in self.object_list:
+        #     if
+        context['readCaret'] = self.request.user.postsReadCaret.all
         context['category'] = self.c
         context['post_pagination'] = PostView.paginate_by
         return context

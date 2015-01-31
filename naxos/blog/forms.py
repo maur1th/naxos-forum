d6acab6f623aa95e5ec86c28d20278fed57c4f33
Thomas Maurin <maurin.th@gmail.com>
2015-01-31 13:11:43 +0100

    Will resize large images

--- naxos/blog/forms.py
+++ naxos/blog/forms.py
@@ -1,10 +1,15 @@
 from django import forms
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from PIL import Image
+from io import BytesIO
 
 from .models import Post
 
+
+IMAGE_SIZE = 800, 450
 
 class PostForm(forms.ModelForm):
 
@@ -12,6 +17,19 @@ class PostForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.add_input(Submit('submit', 'Enregistrer'))
+
+    def clean_image(self, *args, **kwargs):
+        # Resize image if needed
+        form_img = self.cleaned_data['image']
+        image = Image.open(form_img)
+        image.thumbnail(IMAGE_SIZE)
+        # Create a file-like object to write thumb data
+        image_io = BytesIO()
+        image.save(image_io, format='JPEG')
+        # Create a new Django file-like object to be used as ImageField
+        image = InMemoryUploadedFile(image_io, form_img.field_name,
+            form_img.name, 'image/jpeg', len(image_io.getvalue()), None)
+        return image
 
     class Meta:
         model = Post

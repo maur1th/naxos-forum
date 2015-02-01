ccef528ee9f5b35f963d48443699122afb6b0f49
Thomas Maurin <maurin.th@gmail.com>
2015-02-01 16:07:06 +0100

    bugfix

--- naxos/user/models.py
+++ naxos/user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 from django.core.cache import cache
 

6af83e4c64ab9c07694c8e603cfe71bb87f86d55
Thomas Maurin <maurin.th@gmail.com>
2015-06-12 22:45:54 +0200

    Reorganised js code for better clarity

--- naxos/static/js/naxos.js
+++ naxos/static/js/naxos.js
@@ -1,3 +1,4 @@
+// regex
 // http://james.padolsey.com/javascript/regex-selector-for-jquery/
 jQuery.expr[':'].regex = function(elem, index, match) {
 var matchParams = match[3].split(','),
@@ -11,6 +12,7 @@ var matchParams = match[3].split(','),
     regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
 return regex.test(jQuery(elem)[attr.method](attr.property));
 };
+
 // Scroll to caret position in a text field
 function scrollToPosition($textarea, caret) {
     var text = $textarea.val();
@@ -21,8 +23,12 @@ function scrollToPosition($textarea, caret) {
     $textarea.val(text);
     $textarea[0].setSelectionRange(caret, caret);
 };
+
+// Document ready jquery
 $(document).ready(function(){
-  // Ensure presentation is ok on DOM ready()
+
+  // Ensure author div and content div stay the same size
+  // supporting code in grid.js
   $('.equal-divs').responsiveEqualHeightGrid();
   // Ensure presentation is ok while media still loading
   var x = setInterval(function() {
@@ -32,16 +38,48 @@ $(document).ready(function(){
     // Stops presentation refresh when everything loaded
     clearInterval(x);
   });
+
   // Responsive images in posts
   $('.post-content img').load(function() {
       if ($(this).width() > 100) {
         $(this).addClass("img-responsive");
       }
   });
+
   // Remove inner spoiler tags
   $(".panel-body .panel").remove();
-    // Remove inner quotes
+
+  // Remove inner quotes
   $("blockquote blockquote").remove();
+
+  // Animate poll results
+  var $chart = $('.poll-chart');
+  var $bar = $('.poll-bar');
+  var highestNumber = 0;  
+  var sum = 0;
+  // Get highest number and use that as 100%
+  $chart.find($bar).each(function() {
+      var num = parseInt($(this).text(),10);
+      sum += num;
+      if (num > highestNumber) {
+          highestNumber = num;
+      };
+  });
+  // Set the progress bar data
+  $chart.find($bar).each(function() {
+      num = $(this).text();
+      // Convert to percentage and round
+      dispPerc = Math.round((num / highestNumber) * 100);
+      realPerc = Math.round((num / sum) * 100);
+      if(!isNaN(realPerc) && realPerc !== 0) {
+          $(this).animate({width: dispPerc + '%'}, {duration:'fast'});
+          $(this).text(Math.round(realPerc) + '%');
+          $(this).attr("aria-valuenow", dispPerc);
+      } else {
+          $(this).text("");
+      };
+  });
+
   // Ensure presentation is ok while spoiler animation is playing
   // Give spoiler tags unique ids and hyperlink adresses
   $("div[id^='spoiler-panel']").attr("id", function(index) {
@@ -64,7 +102,9 @@ $(document).ready(function(){
       $row.children('.equal-divs').responsiveEqualHeightGrid();
     }, 20);
   });
+
 });
+
 // socket.io
 $(document).ready(function(){
   var socket = io('http://geekattitude.org:80');

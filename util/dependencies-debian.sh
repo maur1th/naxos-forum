3784850d464ad66d739444e5dd77b593e907ff1d
Thomas Maurin <maurin.th@gmail.com>
2015-06-25 13:07:40 +0200

    Updated debian dependencies

--- util/dependencies-debian.sh
+++ util/dependencies-debian.sh
@@ -1,2 +1,1 @@
-sudo apt-get install -y git build-essential python3 nginx postgresql postgresql-server-dev-9.4 memcached libmemcached-dev libtiff5-dev libjpeg62-turbo-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev nginx supervisor python-django python-django-braces
-# python3-pip virtualenvwrapper
+sudo apt-get install -y git build-essential python3 nginx postgresql postgresql-server-dev-9.4 memcached libmemcached-dev libtiff5-dev libjpeg62-turbo-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev nginx supervisor python-django python-django-braces python3-pip virtualenvwrapper

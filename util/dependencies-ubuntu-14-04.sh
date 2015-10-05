d19041f8f1c5f09ca7ce751046044afa24613d80
Thomas Maurin <maur1th@users.noreply.github.com>
2015-10-06 01:16:14 +0200

    added missing dep

--- util/dependencies-ubuntu-14-04.sh
+++ util/dependencies-ubuntu-14-04.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y git build-essential python3 python3-pip virtualenvwrapper nginx postgresql postgresql-server-dev-9.3 memcached libmemcached-dev python-imaging nginx supervisor
+sudo apt-get install -y git build-essential python3 python3-pip virtualenvwrapper nginx postgresql postgresql-server-dev-9.3 memcached libmemcached-dev python-imaging nginx supervisor libjpeg-dev

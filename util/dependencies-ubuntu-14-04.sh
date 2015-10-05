f3ea35bcc79d48d888b5607d31200777653fc8c0
Thomas Maurin <maurin.th@gmail.com>
2015-10-06 01:33:03 +0200

    removed outdated script

--- util/dependencies-ubuntu-14-04.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get install -y git build-essential python3 python3-pip virtualenvwrapper nginx postgresql postgresql-server-dev-9.3 memcached libmemcached-dev python-imaging nginx supervisor libjpeg-dev libtiff5-dev libfreetype6-dev liblcms2-dev libwebp-dev

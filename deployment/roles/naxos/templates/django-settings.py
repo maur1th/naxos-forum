0e266047b1c4961a304057f05aba1821ebbcb3ea
Thomas Maurin <thomas@maurin.io>
2016-10-21 01:32:16 +0200

    refactor(deployment): Merge base and local django setting files into a single ansible template

--- deployment/roles/naxos/templates/django-settings.py
+++ deployment/roles/naxos/templates/django-settings.py
@@ -1,22 +1,106 @@
-# settings/prod.py
-from .base import *
+"""
+Django settings for naxos project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+from .util import root, BASE_DIR
+from .secretKeyGen import SECRET_KEY  # Secret key from generator module
+
 
 DEBUG = {{debug_mode}}
-TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (("{{admin_name}}", "{{admin_email}}"),)
-ROBOT = "{{robot_name}}"
+# Configuring directories
+MEDIA_ROOT = root('media')
+STATICFILES_DIRS = (root('static'),)
 
-SERVER_EMAIL = "{{server_email}}"
-EMAIL_HOST = "{{email_host}}"
-EMAIL_PORT = "{{email_port}}"
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "{{server_email}}"
-EMAIL_HOST_PASSWORD = "{{email_host_password}}"
-EMAIL_SUBJECT_PREFIX = ""
-
-SITE_URL = "http://www.{{allowed_hosts}}"
+# Security
 ALLOWED_HOSTS = ("{{allowed_hosts}}",)
+SITE_URL = "http://www.{{allowed_hosts}}"
+SECRET_KEY = SECRET_KEY
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+
+# App conf
+ADMINS = (("{{admin_name}}", "{{admin_email}}"),)
+INSTALLED_APPS = (
+    # Django Apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
+    # Third-party Apps
+    'crispy_forms',
+
+    # Project Apps
+    'forum',
+    'user',
+    'pm',
+    'blog',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'naxos.urls'
+
+WSGI_APPLICATION = 'naxos.wsgi.application'
+
+LANGUAGE_CODE = 'fr-fr'
+TIME_ZONE = 'Europe/Paris'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+AUTH_USER_MODEL = 'user.ForumUser'
+
+LOGIN_URL = 'user:login'
+LOGIN_REDIRECT_URL = 'forum:top'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            root('templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': DEBUG,
+        },
+    },
+]
 
 DATABASES = {
     "default": {
@@ -57,3 +141,15 @@ LOGGING = {
         "handlers": ["console", "logfile"]
     },
 }
+
+# Email
+SERVER_EMAIL = "{{server_email}}"
+EMAIL_HOST = "{{email_host}}"
+EMAIL_PORT = "{{email_port}}"
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "{{server_email}}"
+EMAIL_HOST_PASSWORD = "{{email_host_password}}"
+EMAIL_SUBJECT_PREFIX = ""
+
+# Misc
+ROBOT = "{{robot_name}}"

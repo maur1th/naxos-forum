a0f243e38f6a8a76f287092cedd471bdb6009740
Thomas Maurin <thomas@maurin.io>
2017-05-21 16:44:02 +0200

    chore: Remove outdated script

--- deployment/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ "$1" = "full" ]]; then
-  ansible-playbook -i prod --vault-password-file ~/.vault_pass site.yml
-else
-  ansible-playbook -i prod --vault-password-file ~/.vault_pass --skip-tags full-run site.yml
-fi

512606d0dd804c2e5d0fc749db0b06e955d8e8b7
Thomas Maurin <thomas@maurin.io>
2017-09-06 01:13:42 +0200

    fix(ansible): Backup script to work with debian-based images

--- deployment/roles/db/templates/entrypoint.sh
+++ deployment/roles/db/templates/entrypoint.sh
@@ -5,9 +5,10 @@ export AWS_ACCESS_KEY_ID={{aws_access_key_id}}
 export AWS_SECRET_ACCESS_KEY={{aws_secret_access_key}}
 
 pg_dumpall -h db -p 5432 -U postgres > db.out
-apk add --no-cache \
-    py2-pip \
-    xz
+apt-get update
+apt-get install -y \
+    python-pip \
+    xz-utils
 pip install awscli
-tar cvJf db-backup-$(date -I'seconds').tar.xz db.out
+tar cvJf db-backup-$(date -I'seconds' | tr : -).tar.xz db.out
 aws s3 cp db-backup-*.tar.xz s3://$S3_BUCKET/db-backup/

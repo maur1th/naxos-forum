f2101533b93f7324d186b4714fe6c62db003dc0a
Thomas Maurin <thomas@maurin.io>
2020-12-31 01:36:11 +0100

    Add back postgres role

---
+++ deployment/roles/postgres/templates/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -ex
+
+export S3_BUCKET={{aws_storage_bucket_name}}
+export AWS_ACCESS_KEY_ID={{aws_access_key_id}}
+export AWS_SECRET_ACCESS_KEY={{aws_secret_access_key}}
+
+pg_dumpall -h db -p 5432 -U postgres > db.out
+apt-get update
+apt-get install -y \
+    python-pip \
+    xz-utils
+pip install awscli
+tar cvJf db-backup-$(date -I'seconds' | tr : -).tar.xz db.out
+aws s3 cp db-backup-*.tar.xz s3://$S3_BUCKET/db-backup/

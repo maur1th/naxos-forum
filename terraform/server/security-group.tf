c65ccc687a90999502e13f40f4dd607e3abb944b
Thomas Maurin <thomas@maurin.io>
2021-01-02 20:30:50 +0100

    [terraform] Add IPV6 SG rules

--- terraform/server/security-group.tf
+++ terraform/server/security-group.tf
@@ -2,6 +2,7 @@ resource "aws_security_group" "this" {
   name   = local.name
   vpc_id = data.aws_vpc.selected.id
 
+  # IPV4
   ingress {
     description = "SSH"
     from_port   = 22
@@ -31,6 +32,38 @@ resource "aws_security_group" "this" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # IPV6
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {

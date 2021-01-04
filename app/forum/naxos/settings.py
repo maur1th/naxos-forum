de703b4b568e064771ef9c5a64c1f7a0163d957e
Thomas Maurin <thomas@maurin.io>
2021-01-04 02:10:24 +0100

    [forum] Fix AWS default ACL

--- app/forum/naxos/settings.py
+++ app/forum/naxos/settings.py
@@ -28,6 +28,7 @@ if os.environ["LOCAL_ENV"] == "1":
     MEDIA_URL = "/media/"
 else:
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    AWS_DEFAULT_ACL = "public-read"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com"

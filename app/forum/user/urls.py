3a3d465485b2d0e0fbecb9761016d8b7bcfc9417
Thomas Maurin <thomas@maurin.io>
2024-06-22 15:55:00 +0200

    Fix urls

--- app/forum/user/urls.py
+++ app/forum/user/urls.py
@@ -21,13 +21,13 @@ urlpatterns = [
             success_url='password_reset_done'),
          name='password_reset'),
     path('password_reset/password_reset_done', auth_views.PasswordResetDoneView.as_view()),
-    re_path((r'reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
+    re_path((r'password_reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
              r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$'),
             auth_views.PasswordResetConfirmView.as_view(
                 form_class=CrispySetPasswordForm,
-                success_url='user:password_reset_complete'),
+                success_url='password_reset_complete'),
             name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+    path('password_reset/password_reset_complete', auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
     path('node_api/', views.node_api, name='node_api'),
 ]

7c3537e418c3e0fe7f0fe9baea8f12de3e5663e2
Thomas Maurin <tmaurin@change.org>
2025-08-09 02:14:48 +0200

    fix backup script

--- deployment/roles/postgres/templates/entrypoint.sh
+++ deployment/roles/postgres/templates/entrypoint.sh
@@ -6,8 +6,7 @@ export S3_BUCKET={{aws_storage_bucket_name}}
 pg_dumpall -h db -p 5432 -U postgres > db.out
 apt-get update
 apt-get install -y \
-    python-pip \
-    xz-utils
-pip install awscli
+    xz-utils \
+    awscli
 tar cvJf db-backup-$(date -I'seconds' | tr : -).tar.xz db.out
 aws s3 cp db-backup-*.tar.xz s3://$S3_BUCKET/db-backup/
